7827a42564f423702c382b13f7a07957f0a82f79
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2018-02-23 15:55:03 -0600

    user cfenv to get uri

--- config.js
+++ config.js
@@ -5,8 +5,7 @@ var appEnv = cfenv.getAppEnv();
 if (appEnv.isLocal) {
   var uri = 'http://localhost:3000';
 } else {
-  var vcaps_services = JSON.parse(process.env.VCAP_SERVICES);
-  var uri = vcaps_services['user-provided'][0].credentials.uri;
+  var uri = appEnv.getServiceURL("user-api-service");
 }
 
 module.exports = {
