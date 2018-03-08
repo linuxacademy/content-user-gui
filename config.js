bd737135eee70f1c6dab4983d3506515d0be0959
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2018-03-08 10:51:16 -0600

    add local env var for newelic

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ var newrelic_license_key, uri;
 
 if (appEnv.isLocal) {
   uri = 'http://localhost:3000';
+  newrelic_license_key = process.env.NEW_REILIC_LICENSE_KEY;
 } else {
   uri = appEnv.getServiceURL("user-api-service");
   newrelic_license_key = appEnv.getServiceURL("newrelic");
