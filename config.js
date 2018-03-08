98adb5e210b53f7679b4d4e047170b1b334a0a58
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2018-03-08 10:48:53 -0600

    add new relic

--- config.js
+++ config.js
@@ -2,12 +2,16 @@
 var cfenv = require("cfenv");
 var appEnv = cfenv.getAppEnv();
 
+var newrelic_license_key, uri;
+
 if (appEnv.isLocal) {
-  var uri = 'http://localhost:3000';
+  uri = 'http://localhost:3000';
 } else {
-  var uri = appEnv.getServiceURL("user-api-service");
+  uri = appEnv.getServiceURL("user-api-service");
+  newrelic_license_key = appEnv.getServiceURL("newrelic");
 }
 
 module.exports = {
   'api_url': uri,
+  'newrelic_license_key': newrelic_license_key,
 }
